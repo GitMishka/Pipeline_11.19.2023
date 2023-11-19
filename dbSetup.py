25451dc2fea663b8889601fa135b666de8baad8a
Misha <mishagrigoryan@gmail.com>
2023-11-19 17:44:23 -0500

    created

---
+++ dbSetup.py
@@ -0,0 +1,24 @@
+
+
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+# Database connection parameters
+params = {
+    'dbname': 'postgres',
+    'user': 'your_username',
+    'password': 'your_password',
+    'host': 'localhost'
+}
+
+conn = psycopg2.connect(**params)
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+cur = conn.cursor()
+
+new_db_name = 'AdventureWorks'
+cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(new_db_name)))
+
+cur.close()
+conn.close()
